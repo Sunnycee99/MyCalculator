64e1bcef8f12b9e02a19f4e13d628f1061faac60
oolyloo <101777278+oolyloo@users.noreply.github.com>
2022-04-01 00:17:15 +0800

    Update baseOperator.py

--- baseOperator.py
+++ baseOperator.py
@@ -1,4 +1,5 @@
 """
+Create by LYL
 base operation model
 """
 
@@ -12,59 +13,104 @@ class BaseOperator:
     3: 相乘
     4: 相除
     """
-    __operator_status = 0   # 私有变量，表示当前需进行的运算操作
-    __data_1 = 0
-    __data_2 = 0
-
+    __operator_status = None   # 私有变量，表示当前需进行的运算操作
+    __data_1 = None
+    __data_2 = None
+    __saved_ans = None
 
     def __add(self):
         """
         两数相加
         """
-        return self.__data_1 + self.__data_2
+        ans = self.__data_1 + self.__data_2
+        self.ans_setter(ans)
+        return 
 
     def __substract(self):
         """
         两数相减
         """
-        return self.__data_1 - self.__data_2
+        ans = self.__data_1 - self.__data_2
+        self.ans_setter(ans)
+        return ans
 
     def __multi(self):
         """
         两数相乘
         """
-        return self.__data_1 * self.__data_2
+        ans = self.__data_1 * self.__data_2
+        self.ans_setter(ans)
+        return ans
+    
+    def __divide(self):
+        """
+        两数相除
+        """
+        ans = self.__data_1 / self.__data_2
+        self.ans_setter(ans)
+        return ans
         
-    def refresh_data_1(new_data, self):
+    def refresh_data_1(self, new_data):
         """
         data_1赋值
         """
         self.__data_1 = new_data
 
-    def refresh_data_2(new_data, self):
+    def data1_getter(self):
+        """
+        获取data1
+        """
+        return self.__data_1
+
+    def refresh_data_2(self, new_data):
         """
         data_2赋值
         """
         self.__data_2 = new_data
 
-    def refresh_status(new_status, self):
+    def data2_getter(self):
+        """
+        获取data2
+        """
+        return self.__data_2
+
+    def refresh_status(self, new_status):
         """
         操作私有变量operator_status
         """
         self.__operator_status = new_status
+    
+    def ans_setter(self, new_ans):
+        """
+        存储计算结果
+        """
+        self.__saved_ans = new_ans
+
+    def ans_getter(self):
+        """
+        取出上一次计算的结果
+        """
+        return self.__saved_ans
+
+    def clearData(self):
+        """
+        清除数据
+        """
+        self.refresh_data_1(None)
+        self.refresh_data_2(None)
+        self.refresh_status(None)
+        self.ans_setter(None)
 
     def base_operation(self):
         """
         调用运算函数,并返回结果
         """
         if self.__operator_status == 1:
-            self.__add(self.__data_1, self.__data_2)
+            return self.__add()
         elif self.__operator_status == 2:
-            self.__substract(self.__data_1, self.__data_2)
+            return self.__substract()
         elif self.__operator_status == 3:
-            self.__multi(self.__data_1, self.__data_2)
-        
+            return self.__multi()
+        elif self.__operator_status == 4:
+            return self.__divide()
 
-
-    
-    
