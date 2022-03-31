29ecc1ea5f634f0cfb56658822f7a1c14b9d99d2
oolyloo <101777278+oolyloo@users.noreply.github.com>
2022-04-01 02:19:52 +0800

    Update seniorOperator.py

--- seniorOperator.py
+++ seniorOperator.py
@@ -9,7 +9,7 @@
 
 """
 
-from app.process.baseOperator import BaseOperator
+from baseOperator import BaseOperator
 
 class seniorOperation(BaseOperator):
     """
@@ -19,43 +19,83 @@ class seniorOperation(BaseOperator):
         """
         幂函数
         """
+        self.__operator_status = 3
         base = self.data1_getter()
         index = self.data2_getter()
-        for i in range(index+1):
-            ex = base * base
+        ex = 1
+        for i in range(index):
+            ex = ex * base
+        self.clearData()
         return ex
     
+    def __factorial(self, num):
+        """
+        阶乘计算
+        """
+        ans = 1
+        for i in range(1, num+1):
+            ans = ans * i
+        return ans
+
     def sin_func(self):
         """
         sin函数
         """
-        ans = 0
+        num = self.data1_getter()
+        ans = 0.0
+        for i in range(1, 13, 4):
+            self.refresh_data_1(num)
+            self.refresh_data_2(i)
+            ans = ans + (self.exponential() / self.__factorial(i))
+            self.clearData()
+        for i in range(3, 15, 4):
+            self.refresh_data_1(num)
+            self.refresh_data_2(i)
+            ans = ans - (self.exponential() / self.__factorial(i))
+            self.clearData()
         return ans
 
     def cos_func(self):
         """
         cos函数
         """
-        ans = 0
+        ans = 0.0
+        num = self.data1_getter()
+        for i in range(0, 12, 4):
+            self.refresh_data_1(num)
+            self.refresh_data_2(i)
+            ans = ans + (self.exponential() / self.__factorial(i))
+            self.clearData()
+        for i in range(2, 14, 4):
+            self.refresh_data_1(num)
+            self.refresh_data_2(i)
+            ans = ans - (self.exponential() / self.__factorial(i))
+            self.clearData()
         return ans
 
     def tan_func(self):
         """
         tan函数
         """
-        ans = 0
+        ans = 0.0
+        num = self.data1_getter()
+        sin_ans = self.sin_func()
+        self.refresh_data_1(num)
+        cos_ans = self.cos_func()
+        ans = sin_ans / cos_ans
         return ans
-"""
-* for test
 
+"""
+ for test
+"""
+"""
 if __name__ == "__main__":
     ca = seniorOperation()
-    ca.refresh_data_1(2)
-    ca.refresh_data_2(2)
-    print(ca.exponential())
+    ca.refresh_data_1(1.024)
+    ca.refresh_data_2(-1)
+    ca.refresh_status(3)
+    print(ca.base_operation())
 """
-
-
 
     
     
