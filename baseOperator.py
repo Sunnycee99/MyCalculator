81fcb628bc01a42a492b7c41ba67b93b7eec9fba
Tylala001 <101779642+Tylala001@users.noreply.github.com>
2022-04-01 16:21:15 +0800

    Update baseOperator.py

--- baseOperator.py
+++ baseOperator.py
@@ -101,16 +101,5 @@ class BaseOperator:
         self.refresh_status(None)
         self.ans_setter(None)
 
-    def base_operation(self):
-        """
-        调用运算函数,并返回结果
-        """
-        if self.__operator_status == 1:
-            return self.__add()
-        elif self.__operator_status == 2:
-            return self.__substract()
-        elif self.__operator_status == 3:
-            return self.__multi()
-        elif self.__operator_status == 4:
-            return self.__divide()
+
 
